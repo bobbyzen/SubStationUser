da0014a1919d4b2c270c4ac361bfeae9461df9d1
bobbyzen <misticclasher@gmail.com>
2020-04-30 16:41:20 +0700

    ALL

---
+++ app/src/main/java/com/bzen/substationuser/BantuanActivity.java
@@ -0,0 +1,16 @@
+package com.bzen.substationuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BantuanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bantuan);
+
+
+    }
+}
--- app/src/main/java/com/bzen/substationuser/MainActivity.java
+++ app/src/main/java/com/bzen/substationuser/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView ivTambah, ivGardu, ivPengaduan, ivBantuan;
+    TextView tvTambah, tvGardu, tvPengaduan, tvBantuan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,63 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        ivTambah = findViewById(R.id.ivTambah);
+        ivTambah.setOnClickListener(this);
+
+        tvTambah = findViewById(R.id.tvTambah);
+        tvTambah.setOnClickListener(this);
+
+        ivGardu = findViewById(R.id.ivGardu);
+        ivGardu.setOnClickListener(this);
+
+        tvGardu = findViewById(R.id.tvGardu);
+        tvGardu.setOnClickListener(this);
+
+        ivPengaduan = findViewById(R.id.ivPengaduan);
+        ivPengaduan.setOnClickListener(this);
+
+        tvPengaduan = findViewById(R.id.tvPengaduan);
+        tvPengaduan.setOnClickListener(this);
+
+        ivBantuan = findViewById(R.id.ivBantuan);
+        ivBantuan.setOnClickListener(this);
+
+        tvBantuan = findViewById(R.id.tvBantuan);
+        tvBantuan.setOnClickListener(this);
+
+        Toast.makeText(this, "AAA", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivTambah :
+                Toast.makeText(this, "Tambah", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tvTambah :
+                Toast.makeText(this, "Tambah", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivGardu :
+                Toast.makeText(this, "Gardu", Toast.LENGTH_SHORT).show();
+                break;
+            case  R.id.tvGardu :
+                Toast.makeText(this, "Gardu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivPengaduan :
+                Toast.makeText(this, "Pengaduan", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvPengaduan :
+                Toast.makeText(this, "Pengaduan", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ivBantuan :
+                Toast.makeText(this, "Bantuan", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvBantuan :
+                Toast.makeText(this, "Bantuan", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
