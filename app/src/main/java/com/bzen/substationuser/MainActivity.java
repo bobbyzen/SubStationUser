6cb5d8cf97908b40c7e86df80bd64612c5dd9661
bobbyzen <misticclasher@gmail.com>
2020-05-09 21:55:09 +0700

    ALL

--- app/src/main/java/com/bzen/substationuser/MainActivity.java
+++ app/src/main/java/com/bzen/substationuser/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,26 +18,27 @@ import com.bumptech.glide.Glide;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageView ivTambah, ivGardu, ivPengaduan, ivBantuan;
+//    ImageView ivTambah, ivGardu, ivPengaduan, ivBantuan;
+    Button btnTambah, btnGardu, btnPengaduan;
     TextView tvTambah, tvGardu, tvPengaduan, tvBantuan;
     SaveSharedPreferences saveSharedPreference;
 
-    ConstraintLayout background;
+//    ConstraintLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         saveSharedPreference = new SaveSharedPreferences();
 
-        background = findViewById(R.id.background);
-        try {
-            Drawable b = Glide.with(this).asDrawable().load(R.drawable.bgc).into(100,100).get();
-            background.setBackground(b);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        background = findViewById(R.id.background);
+//        try {
+//            Drawable b = Glide.with(this).asDrawable().load(R.drawable.bgc).into(100,100).get();
+//            background.setBackground(b);
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         if(saveSharedPreference.getEmail(MainActivity.this).length() == 0){
 
@@ -47,14 +49,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             setContentView(R.layout.activity_main);
 
-            ivTambah = findViewById(R.id.ivTambah);
-            ivTambah.setOnClickListener(this);
+//            ivTambah = findViewById(R.id.ivTambah);
+//            ivTambah.setOnClickListener(this);
+//
+//            ivGardu = findViewById(R.id.ivGardu);
+//            ivGardu.setOnClickListener(this);
+//
+//            ivPengaduan = findViewById(R.id.ivPengaduan);
+//            ivPengaduan.setOnClickListener(this);
 
-            ivGardu = findViewById(R.id.ivGardu);
-            ivGardu.setOnClickListener(this);
+            btnGardu = findViewById(R.id.btnGardu);
+            btnGardu.setOnClickListener(this);
 
-            ivPengaduan = findViewById(R.id.ivPengaduan);
-            ivPengaduan.setOnClickListener(this);
+            btnTambah = findViewById(R.id.btnTambah);
+            btnTambah.setOnClickListener(this);
+
+            btnPengaduan = findViewById(R.id.btnPengaduan);
+            btnPengaduan.setOnClickListener(this);
 
         }
     }
@@ -62,15 +73,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.ivTambah :
+            case R.id.btnTambah :
                 Toast.makeText(this, "Tambah", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(this, MapsActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.ivGardu :
+            case R.id.btnGardu :
                 Toast.makeText(this, "Gardu", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.ivPengaduan :
+            case R.id.btnPengaduan :
                 Toast.makeText(this, "Pengaduan", Toast.LENGTH_SHORT).show();
                 break;
         }
